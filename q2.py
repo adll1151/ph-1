8352711f0494f2107f85170f8210f20e25cc4694
송우영 <124108640+adll1151@users.noreply.github.com>
2023-02-03 15:44:06 +0900

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,9 +1,9 @@
 def solution(numbers):
-    sum = 0
+    total = 0
     for i in range(1,10):
         if i not in numbers:
-            sum += i  
-    return sum
+            total += i  
+    return toal
 numbers = [1,2,3,4,6,7,8,0]
 answer = solution(numbers)
 print(answer)
